c237d64d63eebad451826e09bd241a45cf4d27fe
Frederico Corda <fredericomcorda@gmail.com>
2022-09-19 20:48:32 +0100

    First running

--- sample-code.py
+++ sample-code.py
@@ -1,42 +1,47 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-######## Libraries
+# Libraries
 import sys
 import time
+import os
 
 
-# In[1]:
-
-######### Player
+# Player
 
 player = {
     "name": "unknow",
-    "life": 100
+    "life": 100,
+    "current_room": None
 }
 
 # define rooms and items
-######### WC
-wc_room = {
-    "name": "WC room",
+# toilet
+toilet_room = {
+    "name": "toilet room",
     "type": "room",
 }
 
-door_wc = {
-    "name": "door WC",
+door_toilet = {
+    "name": "door toilet",
     "type": "door",
 }
 
-key_wc = {
-    "name": "key for WC",
+key_toilet = {
+    "name": "key for toilet",
     "type": "key",
-    "target": door_wc,
+    "target": door_toilet,
+}
+
+mirror = {
+    "name": "mirror",
+    "type": "furniture",
 }
 ##########
 
-########## Bedroom
+# Bedroom
 bedroom_room = {
-    "name": "Bedroom",
+    "name": "bedroom",
     "type": "room",
 }
 
@@ -46,7 +51,7 @@ door_bedroom = {
 }
 
 key_bedroom = {
-    "name": "key for Bedroom",
+    "name": "key for bedroom",
     "type": "key",
     "target": door_bedroom,
 }
@@ -56,21 +61,17 @@ bed = {
     "type": "furniture",
 }
 
-mirror = {
-    "name": "Mirror",
-    "type": "furniture",
-}
 
 table = {
-    "name": "Bedside table",
+    "name": "bedside table",
     "type": "furniture",
 }
 
 ##########
 
-########## Living Room
+# Living Room
 livingroom_room = {
-    "name": "Living room",
+    "name": "living room",
     "type": "room",
 }
 
@@ -80,7 +81,7 @@ door_livingroom = {
 }
 
 key_livingroom = {
-    "name": "key for Living room",
+    "name": "key for living room",
     "type": "key",
     "target": door_livingroom,
 }
@@ -96,114 +97,149 @@ couch = {
 }
 ##########
 
-########## Kitchen
+# Kitchen
 
 kitchen_room = {
-    "name": "Kitchen room",
+    "name": "kitchen room",
     "type": "room",
 }
 door_kitchen = {
-    "name": "door Kitchen",
+    "name": "door kitchen",
     "type": "door",
 }
 
 key_kitchen = {
-    "name": "key for WC",
+    "name": "key for toilet",
     "type": "key",
     "target": door_kitchen,
 }
 
-
-###########
-
-########### Outside
-outside = {
-  "name": "outside"
+balcony = {
+    "name": "balcony",
+    "type": "furniture",
 }
 
+microwave = {
+    "name": "microwave",
+    "type": "furniture",
+}
+###########
+
+# Outside
+outside = {"name": "outside"}
 
 
-all_rooms = [wc_room, kitchen_room, bedroom_room, livingroom_room, outside]
+all_rooms = [toilet_room, bedroom_room, livingroom_room, kitchen_room, outside]
 
-all_doors = [door_wc, door_bedroom, door_livingroom, door_kitchen]
+all_doors = [door_toilet, door_bedroom, door_livingroom, door_kitchen]
 
 # define which items/rooms are related
 
 object_relations = {
-    "living room": [couch, piano, door_wc],
+    "toilet room": [mirror, door_toilet],
+    "living room": [couch, piano, door_livingroom],
+    "kitchen room": [balcony, door_kitchen, microwave],
     "bedroom": [bed, mirror, table],
-    "piano": [key_wc],
-    "outside": [door_wc],
-    "door wc": [wc_room, outside]
+    "bed": [key_bedroom],
+    "mirror": [key_toilet],
+    "outside": [door_toilet],
+    "door toilet": [toilet_room, outside],
+    "balcony": [key_kitchen],
+    "couch": [key_livingroom],
 }
 
-# define game state. Do not directly change this dict. 
+# define game state. Do not directly change this dict.
 # Instead, when a new game starts, make a copy of this
-# dict and use the copy to store gameplay state. This 
+# dict and use the copy to store gameplay state. This
 # way you can replay the game multiple times.
 
 INIT_GAME_STATE = {
-    "current_room": wc_room,
+    "current_room": all_rooms[0],
     "keys_collected": [],
-    "target_room": outside
+    "target_room": outside,
 }
 
 
-def print_slow(string, speed=0.02):
-    """This function will write a terminal message in a slow way so that 
+def print_slow(string, speed=0.01):
+    """This function will write a terminal message in a slow way so that
     the user can keep track of what is happening"""
     for letter in string:
         sys.stdout.write(letter)
         sys.stdout.flush()
         time.sleep(speed)
+    linebreak()
 
 
 def linebreak():
     """
     Print a line break
     """
-    print("\n\n")
+    print("\n")
+
 
 def start_game():
     """
     Start the game
     """
-    print_slow("""You wake up on a couch and find yourself in a strange division with no windows which you have never been to before, it's a toilet for sure. You don't remember why you are here and what had happened before...\n You feel some unknown danger is approaching and you must get out of the house, NOW!""")
+    # print_slow(
+    #    """You wake up on a couch and find yourself in a strange division with no windows which you have never been to before, it's a toilet for sure. You don't remember why you are here and what had happened before...\n You feel some unknown danger is approaching and you must get out of the house, NOW!"""
+    # )
+    # get_user_name()
     play_room(game_state["current_room"])
+
 
 def get_user_name():
     """Get player name"""
-    print_slow('You need to focus! try to remenber, what is your name?')
-    player["name"] = input('Right your name here: ')
+    print_slow("You need to focus! try to remenber, what is your name?")
+    player["name"] = input("Right your name here: ")
+    print_slow(
+        f"Yes! my name is {player['name']}! \n Now I need to get out of here!!!")
 
-def play_room(room):
+
+def play_room(room, new_room=True):
     """
     Play a room. First check if the room being played is the target room.
-    If it is, the game will end with success. Otherwise, let player either 
+    If it is, the game will end with success. Otherwise, let player either
     explore (list all items in this room) or examine an item found here.
     """
+    if player["current_room"] is None:
+        player["current_room"] = all_rooms[0]
     game_state["current_room"] = room
-    if(game_state["current_room"] == game_state["target_room"]):
+    if game_state["current_room"] == game_state["target_room"]:
         print_slow("\nYou made it! You escaped the house!")
     else:
-        print_slow("You are now in " + room["name"])
-        intended_action = input("What would you like to do? Type 'explore' or 'examine'?").strip()
-        if intended_action == "explore":
-            explore_room(room)
-            play_room(room)
-        elif intended_action == "examine":
-            examine_item(input("What would you like to examine?").strip())
-        else:
-            print_slow("Not sure what you mean. Type 'explore' or 'examine'.")
-            play_room(room)
-        linebreak()
+        if new_room is True:
+            print_slow(f"I think I'm in {room['name']}... looks like it")
+        print_slow(("What should I do? explore? or examine some object?"))
+        intended_action = None
+        while intended_action is None:
+            intended_action = (
+                input(
+                    "Write 'Explore' to explore all the objects in the room or 'Examine' to examine one of the objects: "
+                ).strip()
+            ).upper()
+            if intended_action == "EXPLORE":
+                explore_room(room)
+                play_room(room, new_room=False)
+            elif intended_action == "EXAMINE":
+                print_slow("What should I examine?")
+                examine_item(
+                    input("Write the object name to examine: ").strip())
+            else:
+                print_slow("I'm confused...")
+                play_room(room, new_room=False)
+            linebreak()
+
 
 def explore_room(room):
     """
     Explore a room. List all items belonging to this room.
     """
     items = [i["name"] for i in object_relations[room["name"]]]
-    print_slow("You explore the room. This is " + room["name"] + ". You find " + ", ".join(items))
+    print_slow(
+        f"Let's explore the {room['name']}. I can see {', '.join(items)}. I should examine them... \n"
+    )
+
 
 def get_next_room_of_door(door, current_room):
     """
@@ -212,14 +248,15 @@ def get_next_room_of_door(door, current_room):
     """
     connected_rooms = object_relations[door["name"]]
     for room in connected_rooms:
-        if(not current_room == room):
+        if not current_room == room:
             return room
+
 
 def examine_item(item_name):
     """
     Examine an item which can be a door or furniture.
     First make sure the intended item belongs to the current room.
-    Then check if the item is a door. Tell player if key hasn't been 
+    Then check if the item is a door. Tell player if key hasn't been
     collected yet. Otherwise ask player if they want to go to the next
     room. If the item is not a door, then check if it contains keys.
     Collect the key if found and update the game state. At the end,
@@ -229,39 +266,46 @@ def examine_item(item_name):
     current_room = game_state["current_room"]
     next_room = ""
     output = None
-    
     for item in object_relations[current_room["name"]]:
-        if(item["name"] == item_name):
-            output = "You examine " + item_name + ". "
-            if(item["type"] == "door"):
+        if item["name"] == item_name:
+            output = "Let's see what I can find in this " + item_name + ". "
+            if item["type"] == "door":
                 have_key = False
                 for key in game_state["keys_collected"]:
-                    if(key["target"] == item):
+                    if key["target"] == item:
                         have_key = True
-                if(have_key):
-                    output += "You unlock it with a key you have."
+                if have_key:
+                    output += "I have a key to unlock this door!"
                     next_room = get_next_room_of_door(item, current_room)
                 else:
-                    output += "It is locked but you don't have the key."
+                    output += "The door is locked and I don't have the key... dammit! I need to examine more objects!"
             else:
-                if(item["name"] in object_relations and len(object_relations[item["name"]])>0):
+                if (
+                    item["name"] in object_relations
+                    and len(object_relations[item["name"]]) > 0
+                ):
                     item_found = object_relations[item["name"]].pop()
                     game_state["keys_collected"].append(item_found)
-                    output += "You find " + item_found["name"] + "."
+                    output += f"Yes! I've found {item_found['name']}. I need to go to the door next!"
+                    next_room = all_rooms[1]
                 else:
-                    output += "There isn't anything interesting about it."
+                    output += "I couldn't find anything interesting here..."
             print_slow(output)
             break
-
-    if(output is None):
-        print_slow("The item you requested is not found in the current room.")
-    
-    if(next_room and input("Do you want to go to the next room? Ener 'yes' or 'no'").strip() == 'yes'):
+    if output is None:
+        print_slow(
+            f"Hm I can't find a {item_name} in this room... have I spell it wrong?")
+    if (
+        next_room
+        and input("Should I go to the next room? Enter 'yes' or 'no'").strip() == "yes"
+    ):
+        del all_rooms[0]
         play_room(next_room)
     else:
-        play_room(current_room)
+        play_room(current_room, new_room=False)
 
 
 game_state = INIT_GAME_STATE.copy()
 
+os.system("cls" if os.name == "nt" else "clear")
 start_game()
